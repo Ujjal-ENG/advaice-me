5acc8a6af73beb0774161d317e9a709c7651160b
user <ujjalroy7862@gmail.com>
2023-03-11 02:52:19 +0600

    done the advaice git in react

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
+export default class App extends Component {
+    state = {
+        advice: ''
+    };
 
-const App = () => {
-    return (
-        <>
-            <h1 className="text-3xl">Hello</h1>
-        </>
-    );
-};
+    componentDidMount() {
+        this.fetchAvaice();
+        console.log('kop');
+    }
 
-export default App;
+    fetchAvaice = async () => {
+        const result = await axios.get('https://api.adviceslip.com/advice');
+        const { advice } = result.data.slip;
+        this.setState({ advice });
+    };
+    render() {
+        return (
+            <div className="hello h-screen flex justify-center flex-col items-center">
+                <div className="text-black text-4xl  bg-white w-1/2 text-center mx-auto p-20 rounded-xl shadow-lg flex justify-center flex-col gap-6">
+                    {this.state.advice && this.state.advice}
+                    <button className=" px-5 py-3 bg-red-600 w-1/2 mx-auto text-xl font-bold text-white rounded-lg cursor-pointer active:px-3 active:py-2 " onClick={this.fetchAvaice}>
+                        <span>Give ME Advice!!</span>
+                    </button>
+                </div>
+                ;
+            </div>
+        );
+    }
+}
